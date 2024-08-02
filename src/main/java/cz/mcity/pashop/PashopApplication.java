a968e2d28eadcebbae218d85f2c3d29dfe7290f8
Tomas Musil <tomas.musil@mcity.cz>
2024-08-02 17:39:52 +0200

    Initial commit after creation

---
+++ src/main/java/cz/mcity/pashop/PashopApplication.java
@@ -0,0 +1,13 @@
+package cz.mcity.pashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PashopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PashopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cz/mcity/pashop/PashopApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.mcity.pashop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PashopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
